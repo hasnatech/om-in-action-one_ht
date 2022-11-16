8c97a22e20acaf9cde0d85e4134e3568b64b039b
Fashid <fashid@hasnatech.onmicrosoft.com>
2022-11-16 19:04:29 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { security } from 'ht-security';
+import { AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    security({
+      mode: "development"
+    })
+  }
   title = 'om-in-action-one';
+
 }
